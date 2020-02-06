b775105a73a521cd6694eb9f91a6d68f1d348e6e
skrkittu <48560727+skrkittu@users.noreply.github.com>
2020-02-06 14:58:53 -0500

    Update Main3Activity.java

--- app/src/main/java/com/example/quizproject/Main3Activity.java
+++ app/src/main/java/com/example/quizproject/Main3Activity.java
@@ -2,13 +2,48 @@ package com.example.quizproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class Main3Activity extends AppCompatActivity {
+    RadioButton r1,rb,r2,r3;
+    RadioGroup rg;
+    Button b1;
+    String a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        r1=findViewById(R.id.radioButton222);
+        r2=findViewById(R.id.radioButton223);
+        r3=findViewById(R.id.radioButton224);
+        b1=findViewById(R.id.button172);
+        rg=findViewById(R.id.radioGroup11);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int radioid=rg.getCheckedRadioButtonId();
+                rb=findViewById(radioid);
+                if (r2 == rb){
+
+                    Toast.makeText(Main3Activity.this,"CORRECT ANSWER :)",Toast.LENGTH_LONG).show();
+                }
+                else {
+
+                    Toast.makeText(Main3Activity.this,"WRONG ANSWER :( ",Toast.LENGTH_LONG).show();
+                }
+                Intent i =new Intent(Main3Activity.this,Main3Activity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
