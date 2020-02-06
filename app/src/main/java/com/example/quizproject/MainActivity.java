54608960b028c0b254759ef3d4b11ad3f6cbf9e6
Nithin4536 <48560733+Nithin4536@users.noreply.github.com>
2020-02-06 14:07:04 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/quizproject/MainActivity.java
+++ app/src/main/java/com/example/quizproject/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.quizproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Button b1;
+Intent i;
+    TextView t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.button);
+        t1=findViewById(R.id.textView8);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            String a=t1.getText().toString();
+            i=new Intent(MainActivity.this,Main2Activity.class);
+                Toast.makeText(MainActivity.this,"Welcome  "+a,Toast.LENGTH_LONG).show();
+                startActivity(i);
+            }
+        });
     }
 }
+
